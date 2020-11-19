534a3a14adb507de4556a4d32147cc294d8b1717
lucyefranco <lucyefranco@gmail.com>
2020-11-19 15:05:43 -0800

    completes search functionality

--- src/components/BreweryPost.js
+++ src/components/BreweryPost.js
@@ -1,1 +1,17 @@
 // Comment Card!
+import React from 'react';
+
+const BreweryPost = (props) => {
+    return (
+        <div>
+            <div className="Brewerypost">
+                <h5>{ props.name }</h5>
+                <h5>{ props.city },{ props.state }</h5>
+                <h5>{ props.rating }/5</h5>
+                <p>{ props.comment }</p>
+        </div>
+        </div>
+    );
+}
+
+export default BreweryPost;
