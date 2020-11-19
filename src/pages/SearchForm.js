f8e6f3e9c42e33a4086fe26e4fd6b87142b92725
Caseykins <caseyjenkins92@gmail.com>
2020-11-19 13:09:18 -0700

    added component to SearchForm - not working yet. Need to plug url in with interpolation.

--- src/pages/SearchForm.js
+++ src/pages/SearchForm.js
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchForm = () => {
-    return (
-        <div>
-            <form>
-                <input placeholder="Search beers or breweries" />
-                <button>Beer Me!</button>
-            </form>
-        </div>
-    )
+import BrewerySearchModel from '../models/brewerysearch';
+import BreweryCard from '../components/BreweryCard';
+
+// need to fetch from backend to use in search form
+//   pull breweries based on location (city)
+// import brewery list
+//   redirect to brewery list once search is performed
+
+
+class SearchForm extends Component {
+
+    state = {
+        breweries: []
+    }
+
+    componentDidMount() {
+        this.fetchData()
+    }
+
+    fetchData = () => {
+        BrewerySearchModel.all().then(data => {
+            console.log(data)
+            this.setState({ breweries: data})
+        })
+    }
+
+    render() {
+        let breweryList = this.state.breweries && this.state.breweries.map((brewery, index) => {
+            return (
+                <BreweryCard {...brewery} key={index}/>
+            )
+        })
+        
+        return (
+            <div>
+                <form>
+                    <input 
+                    type="text"
+                    // value=""
+                    placeholder="Where are you located?"
+                    // onChange={}
+                    />
+                    <button>Beer Me!</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default SearchForm;
