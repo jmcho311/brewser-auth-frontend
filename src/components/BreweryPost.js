16698edb31e9f65febc637da8a0de3423f7f7c27
lucyefranco <lucyefranco@gmail.com>
2020-11-19 15:17:50 -0800

    adds comments on to brewery show page

--- src/components/BreweryPost.js
+++ src/components/BreweryPost.js
@@ -5,8 +5,6 @@ const BreweryPost = (props) => {
     return (
         <div>
             <div className="Brewerypost">
-                <h5>{ props.name }</h5>
-                <h5>{ props.city },{ props.state }</h5>
                 <h5>{ props.rating }/5</h5>
                 <p>{ props.comment }</p>
         </div>
