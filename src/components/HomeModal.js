0a60c8cc0e63ce94a159cce96284b6a502c85e27
Caseykins <caseyjenkins92@gmail.com>
2020-11-21 01:15:59 -0700

    Modal changes and spacing code out in SearchForm.js

--- src/components/HomeModal.js
+++ src/components/HomeModal.js
@@ -12,25 +12,25 @@ export default class HomeModal extends Component {
         return (
             <div className="homeModal">
 
-                <div className="imgContainer">
+                {/* <div className="imgContainer">
                     <img src="https://i.imgur.com/5y6Jp8c.png?1" alt="beer" id="modalImg" />
+                </div> */}
+                <div className="modalContents">
+                    {/* <h2>Welcome to Brewser!</h2> */}
+
+                    <h5> Are you least 21 years of age?</h5>
+                    
+                    <button className="modalBtn"
+                        onClick={ this.props.onClose } >
+                            Yes, I am!
+                    </button>
+
+                    <button className="modalBtn">
+                        <a href="https://us.teletubbies.com/" id="modalBtn"> 
+                            I wish!
+                        </a>
+                    </button>
                 </div>
-
-                <h2>Welcome to Brewser!</h2>
-
-                <h5> Are you least 21 years of age?</h5>
-                
-                <button className="modalBtn"
-                    onClick={ this.props.onClose } >
-                        Yes, I am!
-                </button>
-
-                <button className="modalBtn">
-                    <a href="https://us.teletubbies.com/" id="modalBtn"> 
-                        I wish!
-                    </a>
-                </button>
-
             </div>
         )
     }
