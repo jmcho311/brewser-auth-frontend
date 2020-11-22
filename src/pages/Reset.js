8842fe8925ace186498aa355f8f797fc84130959
Caseykins <caseyjenkins92@gmail.com>
2020-11-21 17:42:28 -0700

    adds a reset function to clear local storage, which allows modal to pop up again upon page load.

---
+++ src/pages/Reset.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+const Reset = () => {
+    window.localStorage.clear()
+    return (
+        <Redirect to='/' />
+    )
+}
+
+export default Reset;
