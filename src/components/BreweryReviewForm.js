bf5a33705db11bad97569e450c87cd73114908c6
lucyefranco <lucyefranco@gmail.com>
2020-11-23 19:21:01 -0800

    create function worksgit add -A

--- src/components/BreweryReviewForm.js
+++ src/components/BreweryReviewForm.js
@@ -3,30 +3,14 @@ import React, {Component} from 'react';
 class BreweryReviewForm extends Component {
 
     state = {
-        breweryId: this.props.brewery.id,
-        name: this.props.brewery.name,
-        city: this.props.brewery.city,
-        state: this.props.brewery.state,
         rating: 0,
         comment: '',
     }
 
     handleSubmit = (e) => {
         e.preventDefault()
-        this.setState({
-            // breweryId: e.target.breweryId.value,
-            // name: e.target.name.value,
-            // city: e.target.city.value,
-            // state: e.target.state.value,
-            rating: e.target.rating.value,
-            comment: e.target.comment.value,
-        })
-        console.log(this.state.rating)
+        console.log('⭐️', this.state)
         this.props.createPost(
-            this.state.breweryId, 
-            this.state.name,
-            this.state.city,
-            this.state.state,
             this.state.rating, 
             this.state.comment,
             )
@@ -46,46 +30,24 @@ class BreweryReviewForm extends Component {
             </div>
             <form onSubmit={ this.handleSubmit }>
                 <input
-                // userId - import from user model
-                    type="hidden"
-                    name='userId'
-                    value={localStorage.getItem('id')}
-                    />
-                <input
-                // breweryId - import from brewerysearch model
-                    type="hidden"
-                    name = "breweryId"
-                    value={this.props.brewery.id}
-                />
-                <input
-                // name - import from brewerysearch model
-                    type="hidden"
-                    name = "name"
-                    value={this.props.brewery.name}
-                />
-                <input
-                // city - import from brewerysearch model
-                    type="hidden"
-                    name = "city"
-                    value={this.props.brewery.city}
-                />
-                <input
-                // state - import from brewerysearch model
-                    type="hidden"
-                    name = "state"
-                    value={this.props.brewery.state}
-                />
-                <input
                 // rating - user input
                     name = "rating"
                     placeholder="rating"
                     type="integer"
+                    onChange={ (e) => {
+                        this.setState({ rating: e.target.value })
+                    }}
+                    value= {this.state.rating}
                 />
                 <input
                 // comment - user input
                     name = "comment"
                     placeholder="comment"
                     type="text"
+                    onChange={ (e) => {
+                        this.setState({ comment: e.target.value })
+                    }}
+                    value= {this.state.comment}
                 />
                 <button 
                     //onClick={this.props.onClose}
