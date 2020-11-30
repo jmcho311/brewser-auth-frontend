b95ec0980bf02af2bd59265f288d8d212d4132fc
Caseykins <caseyjenkins92@gmail.com>
2020-11-29 22:45:12 -0700

    gives home/search page total makeover

--- src/pages/SearchForm.js
+++ src/pages/SearchForm.js
@@ -66,48 +66,44 @@ class SearchForm extends Component {
 
                 <div className="Page">
                     <h1>Happy Brewsing!</h1>
-                    <img src="https://images.pexels.com/photos/159291/beer-machine-alcohol-brewery-159291.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" id="searchImg" alt="pic" />
-                    <h4>Find breweries by name or location!</h4>
-                    <div className="formBlock">
-                        <form onSubmit={ this.searchBreweries }>
-                            <input 
-                                id="searchbar"
-                                type="text"
-                                placeholder="Find Brewery"
-                                onChange={ this.handleChange }
-                                value={ this.state.city }
-                            />
-                                <button id="searchBtn"> I need a drink! </button>
-                        </form>
-
-                        {/* <form onSubmit={ this.searchBeers }>
-                            <input 
-                            id="searchbar"
-                            type="text"
-                            placeholder="Find Beer"
-                            onChange={ this.handleBeerChange }
-                            value={ this.state.category }
-                            /> */}
-
-                        <form onSubmit={ this.searchBeers }>
-                            <select 
-                                value={ this.state.category } 
-                                onChange={ this.handleBeerChange }
-                                id="selectBar"
-                            >
-                                <option value="--">Select Beer By Category</option>
-                                <option value="Ale">Ale</option>
-                                <option value="Belgian">Belgian</option>
-                                <option value="Cider">Cider</option>
-                                <option value="IPA">IPA</option>
-                                <option value="Lager">Lager</option>
-                                <option value="Pilsner">Pilsner</option>
-                                <option value="Porter">Porter</option>
-                                <option value="Stout">Stout</option>
-                                <option value="Wheat">Wheat</option>
-                            </select>
-                            <button id="searchBtn"> Pour me a pint! </button>
-                        </form>
+                    <div className="imgBlock">
+                        <div className="formBlock">
+                            <div className="form">
+                                <h4 className="formTitle">Find breweries by name or location!</h4>
+                                <form onSubmit={ this.searchBreweries }>
+                                    <input 
+                                        id="searchbar"
+                                        type="text"
+                                        placeholder="Find Brewery"
+                                        onChange={ this.handleChange }
+                                        value={ this.state.city }
+                                    />
+                                        <button id="searchBtn"> I need a drink! </button>
+                                </form>
+                            </div>
+                            <div className="form">
+                                <h4 className="formTitle">Search beer reviews</h4>
+                                <form onSubmit={ this.searchBeers }>
+                                    <select 
+                                        value={ this.state.category } 
+                                        onChange={ this.handleBeerChange }
+                                        id="selectBar"
+                                    >
+                                        <option value="--">Select Beer By Category</option>
+                                        <option value="Ale">Ale</option>
+                                        <option value="Belgian">Belgian</option>
+                                        <option value="Cider">Cider</option>
+                                        <option value="IPA">IPA</option>
+                                        <option value="Lager">Lager</option>
+                                        <option value="Pilsner">Pilsner</option>
+                                        <option value="Porter">Porter</option>
+                                        <option value="Stout">Stout</option>
+                                        <option value="Wheat">Wheat</option>
+                                    </select>
+                                    <button id="searchBtn"> Pour me a pint! </button>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
